8c3e951a9c1eebe7e141152d0fe559a6e5768aac
Evaaaaaaaxiao <328006183@qq.com>
2020-01-20 17:30:28 +0800

    homeicons

---
+++ project/src/pages/home/components/Icons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .icons
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    overflow: hidden
+    .icon
+      float: left
+      width: 25%
+      padding-bottom: 25%
+</style>
